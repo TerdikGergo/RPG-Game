2174710dbef036811f73802e96233622b10fd1cf
TerdikGergo <145001267+TerdikGergo@users.noreply.github.com>
2024-02-08 10:01:56 +0100

    A Karakter Class létrehozása, alap window design

---
+++ Character_Generation_and_Modification/rpg_game/rpg_game/Karakter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg_game
+{
+    class Karakter
+    {
+        //A karakter neve
+        public string Character_name { get; set; }
+        //Alap támadások ereje
+        public int Strength { get; set; }
+        //A karakter hpja
+        public int Vigor { get; set; }
+        //A karakter movementspeedje (Szerintem legyen fixalt ertek, nem latok sok hasznot harc kozben, javaslatokat elfogadok!!!)
+        public int Agility { get; set; }
+        //A karakter crit eselye %-ban
+        public int Crit_chance { get; set; }
+        //Ennyi képességet tud hordozni a karakter
+        public int Knowledge { get; set; }
+        //Milyen tipusu a karakter
+        public string Character_type { get; set; }
+    }
+}
